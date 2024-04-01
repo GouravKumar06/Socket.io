74b0ae61af61a3f4128cd3734e7f74f636328295
GouravKumar06 <gka98963@gmail.com>
2024-04-02 00:23:53 +0530

    second changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React, { useEffect, useMemo, useState } from 'react'
+import {io} from 'socket.io-client'
+import { Container, TextField, Typography, Button } from '@mui/material'
 const App = () => {
+  const [message, setMessage] = useState('')
+
+  const socket = useMemo( () => io('http://localhost:5000'),[]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('message', message);
+  }
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log("connected",socket.id)
+    })
+
+    socket.on('welcome', (msg) => {
+      console.log(msg)
+    })
+
+    socket.on("receive-message", (data) => {
+      console.log(data)
+    })
+
+    return () => { 
+      socket.disconnect()
+    }
+  },[])
   return (
-    <div>App</div>
+    <Container>
+      <Typography variant='h1' component='div'>
+        Welcome to Socket.io
+      </Typography>
+
+      <form onSubmit={handleSubmit}>
+        <TextField onChange={(e) => setMessage(e.target.value)} value={message} id="outlined-basic" label="Outlined" variant="outlined" />
+        <Button type="submit" variant="contained" color="primary">Send</Button>
+      </form>
+    </Container>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,23 +1,44 @@
 import express from 'express';
 import {createServer} from 'http';
 import {Server} from 'socket.io';
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app)
 
-const io = new Server(server)
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
+        credentials:true
+    }
+})
 
 io.on('connection',(socket)=>{
     console.log('a user connected');
     console.log("Id",socket.id)
+
+    // socket.emit("welcome",`Hello from ${socket.id}`)
+    // socket.broadcast.emit("welcome",`A new user with id ${socket.id} has joined`)
+
+    socket.on("message",(data)=>{
+        console.log(data)
+        io.emit("receive-message",data)
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("user disconnected with id from server",socket.id)
+    })
 })
 
-const port = 3000;
+app.use(cors());
+
+const port = 5000;
 
 app.get('/',(req,res)=>{
     res.send('hello world');
 })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`listening on port ${port}`);
 })
